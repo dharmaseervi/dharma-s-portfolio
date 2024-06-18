95cf6516951eb36f300ccad911b6a44f45ce2232
dharmaseervi <dharmaseervi@dharmaseervis-MacBook-Pro.local>
2024-06-18 19:15:02 +0530

    bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <link rel="icon" href="icons/dharma.jpeg" sizes="any" />
         <Providers>
           <NavbarMenu />
           {children}
