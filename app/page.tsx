d6ae52c5e970427fff1788005acdf98624245186
dharmaseervi <dharmaseervi@dharmaseervis-MacBook-Pro.local>
2024-07-02 19:06:22 +0530

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { TracingBeamDemo } from './components/home/blog';
 import ToolExperince from './components/home/experince';
+import Grid from './components/home/grid';
 import HeroSection from './components/home/homeHeroSection'
 import Project from './components/home/project';
 import { InfiniteMovingCardsDemo } from './components/home/techstackCard';
@@ -9,7 +10,7 @@ export default function Home() {
   return (
     <main className="relative dark:bg-black-100 bg-white   ">
       <HeroSection />
-      {/* <Grid /> */}
+      <Grid />
       <ToolExperince />
       <Project />
       <section id="blog">
